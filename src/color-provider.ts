87b26504af6bc324233ff0967f057104cc5b89b1
Shub <shub1493biswas@gmail.com>
2022-02-10 08:13:24 +0530

    feat: show colors for exisiting var() text
    - fix: vscode test mock

--- src/color-provider.ts
+++ src/color-provider.ts
@@ -8,7 +8,6 @@ import {
   EndOfLine,
   Position,
 } from "vscode";
-import fs from "fs";
 import { CACHE } from "./constants";
 import { isObjectEmpty } from "./utils";
 import { setup } from "./main";
@@ -16,96 +15,70 @@ import { parseFiles } from "./parser";
 import { parseToRgb } from "polished";
 import type { RgbaColor } from "polished/lib/types/color";
 
+const getChunkRange = (startLineNumber: number, endLineNumber: number): Range =>
+  new Range(new Position(startLineNumber, 0), new Position(endLineNumber, 0));
 export class CssColorProvider implements DocumentColorProvider {
   async provideDocumentColors(
     document: TextDocument
   ): Promise<ColorInformation[]> {
-    return new Promise(res => {
-      (async () => {
-        if (isObjectEmpty(CACHE.cssVars)) {
-          const { config } = await setup();
-          // Create Cache if not present already.
-          await parseFiles(config);
-        }
-      })().then(() => {
-        const eol = document.eol === EndOfLine.CRLF ? "\r\n" : "\n";
-        const colorInfo: ColorInformation[] = [];
-        /**
-         * FIXME: document.getText() contains unsaved text as well.
-         * This is crucial, as the range keeps on changing for the Color Info
-         * Not sure why but VScode doesn't provide any Buffer instead of text for
-         * huge file content.
-         * That means I will have to work upon the entire text all at once.
-         */
-        const readable = fs.createReadStream(document.uri.fsPath);
-        let lineOffset = 0;
-        let charOffset = 0;
+    if (isObjectEmpty(CACHE.config)) {
+      // Config won't change a lot of time.
+      await setup();
+    }
+    await parseFiles(CACHE.config);
 
-        readable.on("data", (chunk: Buffer) => {
-          const text = chunk.toString();
-          const lineDetails = text
-            .split(eol)
-            .reduce((lineDetails, line, index) => {
-              let charOffset = 0;
-              if (index > 0) {
-                charOffset =
-                  lineDetails[index - 1].length +
-                  lineDetails[index - 1].charOffset;
-              }
-              lineDetails.push({
-                start: new Position(index + lineOffset, 0),
-                charOffset,
-                length: line.length + eol.length,
-              });
-              return lineDetails;
-            }, [] as { start: Position; charOffset: number; length: number }[]);
+    const eol = document.eol === EndOfLine.CRLF ? "\r\n" : "\n";
+    const colorInfo: ColorInformation[] = [];
+    /**
+     * FIXME: document.getText() contains unsaved text as well.
+     * This is crucial, as the range keeps on changing for the Color Info
+     * Not sure why but VScode doesn't provide any Buffer instead of text for
+     * huge file content.
+     * That means I will have to work upon the entire text all at once.
+     */
 
-          const matches = text.matchAll(/var\((.*?)\)/g);
-          for (const cssVarMatch of matches) {
-            const match = cssVarMatch[0];
-            const key = cssVarMatch[1];
-            if (CACHE.cssVarsMap[key]?.color && cssVarMatch.index) {
-              const start = cssVarMatch.index + charOffset;
-              const line = lineDetails.find(
-                detail =>
-                  detail.charOffset <= start &&
-                  start < detail.charOffset + detail.length
-              );
-              if (line) {
-                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                const color = parseToRgb(
-                  CACHE.cssVarsMap[key].color!
-                ) as RgbaColor;
-                const info = {
-                  color: new Color(
-                    color.red / 255,
-                    color.green / 255,
-                    color.blue / 255,
-                    color.alpha ? color.alpha / 255 : 1
-                  ),
-                  range: new Range(
-                    line.start.translate({
-                      characterDelta: start - line.charOffset,
-                    }),
-                    line.start.translate({
-                      characterDelta: start - line.charOffset + match.length,
-                    })
-                  ),
-                };
-                colorInfo.push(info);
-              }
-            }
+    // Assuming the worst case that each line has 120 characters (for JS like files)
+    // 500 lines will contain at-max 500 * 120 = 60000 chars, which is very close to
+    // 2^16 = 65536 max bytes provided by stream chunks.
+    const linesRead = 500;
+    let lineOffset = 0;
+    let text = document.getText(
+      getChunkRange(lineOffset, lineOffset + linesRead)
+    );
+    while (text) {
+      const lines = text.split(eol);
+      lines.pop(); // Last line will be handled in next loop
+      lines.forEach((line, lineNumer) => {
+        const matches = line ? line.matchAll(/var\s*\((.*?)\)/g) : [];
+        const exactLineNumber = lineNumer + lineOffset;
+        for (const cssVarMatch of matches) {
+          const match = cssVarMatch[0];
+          const key = cssVarMatch[1].trim();
+          const hexColor = CACHE.cssVarsMap[key]?.color;
+          if (hexColor && cssVarMatch.index) {
+            const color = parseToRgb(hexColor) as RgbaColor;
+            const info = {
+              color: new Color(
+                color.red / 255,
+                color.green / 255,
+                color.blue / 255,
+                color.alpha ?? 1
+              ),
+              range: new Range(
+                new Position(exactLineNumber, cssVarMatch.index),
+                new Position(exactLineNumber, cssVarMatch.index + match.length)
+              ),
+            };
+            colorInfo.push(info);
           }
-
-          lineOffset = lineDetails.length;
-          charOffset = text.length;
-        });
-        readable.on("end", () => {
-          // console.log(">>>> ", colorInfo);
-          res(colorInfo);
-        });
+        }
       });
-    });
+      lineOffset += linesRead;
+      text = document.getText(
+        getChunkRange(lineOffset, lineOffset + linesRead)
+      );
+    }
+    return colorInfo;
   }
 
   async provideColorPresentations(
