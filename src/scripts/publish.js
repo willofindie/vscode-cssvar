7b799aed25d67e9e51b955f895c0db3f3af9db82
Shub <shub1493biswas@gmail.com>
2022-09-03 21:59:07 +0530

    feat: dual publish extension to vsce & ovsx
    - adds a script to spawn commands and publish to both distributors

---
+++ src/scripts/publish.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+// @ts-check
+const { spawn } = require("node:child_process");
+require("dotenv").config();
+
+const spawnAsync = (command, ...args) =>
+  new Promise((res, rej) => {
+    const thread = spawn(command, args, {
+      env: process.env,
+      stdio: "inherit",
+    });
+
+    thread.on("close", code => {
+      if (code !== 0) {
+        rej(code);
+      }
+      res(0);
+    });
+  });
+
+Promise.all([spawnAsync("yarn", "test"), spawnAsync("yarn", "build")])
+  .then(([c1, c2]) => {
+    console.log("Results: ", c1, c2);
+    return Promise.allSettled([
+      spawnAsync("vsce", "publish"),
+      spawnAsync("ovsx", "publish", "-p", process.env.OVSX_KEY),
+    ]);
+  })
+  .catch((reason)=> {
+    console.error(reason);
+    process.exit(1);
+  });
